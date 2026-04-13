d29199fe7c69f60ae8c98d4ce6ac8d156679198e
sea-brenn <aguilarbrenda19@gmail.com>
2026-04-13 06:28:54 +0000

    implement return to the get_range function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def get_domain(mapping: dict) -> set:
     # === TODO ===
     # Your code here
     return set(mapping.keys())
+    pass
     # === END TODO ===
 
 
@@ -13,6 +14,7 @@ def get_range(mapping: dict) -> set:
     """Return the range — the set of outputs actually mapped to."""
     # === TODO ===
     # Your code here
+    return set(mapping.values())
     pass
     # === END TODO ===
 
